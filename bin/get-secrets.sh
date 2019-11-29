e1471b0703b6c7dddae62660b14948186df68b5c
Alex Ware <alex.ware@guardian.co.uk>
2019-11-29 11:21:14 +0000

    worse

--- bin/get-secrets.sh
+++ bin/get-secrets.sh
@@ -1,20 +1,4 @@
 #!/usr/bin/env node
-var AWS = require('aws-sdk');
-var s3 = new AWS.S3();
-
 var bucket = process.argv[2];
 
-console.log(JSON.stringify(bucket.split("")))
-
-s3.getObject({
-	Bucket: bucket,
-	Key: 'config.json'
-}, function (err, data) {
-	if (err) {
-		console.error(err);
-		process.exit(1);
-	} else {
-		console.log(data.Body.toString('utf8'));
-		process.exit(0);
-	}
-});
+console.log(JSON.stringify(bucket.split(""))
